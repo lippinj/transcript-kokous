1d3e53c2f9f276a6ed6938fd2d9b9bc5c0ff76a8
Joonas Lipping <joonas@scandit.com>
2025-09-21 18:54:45 +0300

    Refactor load

--- kirjuri/load.py
+++ kirjuri/load.py
@@ -1,29 +1,65 @@
 import json
 import os
 
+from dataclasses import dataclass
+
 from youtube_transcript_api import YouTubeTranscriptApi
 
-from .raw import Transcript
-
-
-CACHE_DIR = ".kirjuri-cache"
-
-
-def load_raw(video_id: str, languages: list[str] | None = None) -> list | dict:
-    languages = languages or ["fi"]
-    os.makedirs(CACHE_DIR, exist_ok=True)
-    filepath = f"{CACHE_DIR}/{video_id}-{'-'.join(languages)}.json"
-    try:
-        with open(filepath, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except OSError:
-        api = YouTubeTranscriptApi()
-        result = api.fetch(video_id, languages=languages)
-        raw = result.to_raw_data()
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(raw, f, ensure_ascii=False, indent=2)
-        return raw
+from .raw import Snippet, Transcript
+from .video import Video
 
 
 def load(video_id: str) -> Transcript:
-    return Transcript.from_json(video_id, load_raw(video_id))
+    """Load a raw transcript (from cache if possible)"""
+    spec = FetchSpec(video_id, ["fi"])
+    snippets = Snippet.from_json_list(_load_raw(spec))
+    return Transcript.from_json(Video(video_id), snippets)
+
+
+# Implementation details below
+
+_CACHE_DIR = ".kirjuri-cache"
+
+
+@dataclass
+class FetchSpec:
+    id: str
+    languages: list[str]
+
+    @property
+    def basename(self):
+        return "-".join([self.id] + self.languages)
+
+    @property
+    def filename(self):
+        return f"{self.basename}.json"
+
+    @property
+    def cache_path(self):
+        return f"{_CACHE_DIR}/{self.filename}"
+
+
+def _load_raw(spec: FetchSpec):
+    try:
+        return _load_from_cache(spec)
+    except OSError:
+        raw = _fetch(spec)
+        _store_to_cache(raw, spec)
+        return raw
+
+
+def _load_from_cache(spec: FetchSpec):
+    with open(spec.cache_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def _store_to_cache(j, spec: FetchSpec):
+    os.makedirs(_CACHE_DIR, exist_ok=True)
+    with open(spec.cache_path, "w", encoding="utf-8") as f:
+        json.dump(j, f, ensure_ascii=False, indent=2)
+
+
+def _fetch(spec: FetchSpec):
+    api = YouTubeTranscriptApi()
+    result = api.fetch(spec.id, languages=spec.languages)
+    return result.to_raw_data()
---
+++ kirjuri/video.py
@@ -0,0 +1,19 @@
+class Video:
+    def __init__(self, id: str):
+        self.id = id
+
+    def __str__(self):
+        return self.id
+
+    def __repr__(self):
+        return self.id
+
+    @property
+    def base_url(self):
+        return f"https://www.youtube.com/watch?v={self.video_id}"
+
+    def url(self, timestamp: int | None = None):
+        if timestamp:
+            return f"{self.base_url}&t={timestamp}"
+        else:
+            return self.base_url
